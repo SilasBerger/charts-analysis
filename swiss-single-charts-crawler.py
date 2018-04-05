be160e01976b083be574a9f5fac774b9f2345242
Silas Berger <berger.silu@hotmail.com>
2018-04-05 16:03:35 +0200

    swiss single charts: crawler can fetch all weeks and append to existing data file

--- swiss-single-charts-crawler.py
+++ swiss-single-charts-crawler.py
@@ -29,7 +29,7 @@ def collect_tags(tag, collected_tags=None, br_counter=0):
     elif not str(next_tag) == "\n":
         br_counter = 0
 
-    if br_counter >= 3:
+    if br_counter >= 2:
         return collected_tags
     else:
         return collect_tags(next_tag, collected_tags, br_counter)
@@ -48,30 +48,49 @@ def clean_tags(collected_tags):
 
 
 def parse_entry(entry_string):
+    # parse chart placement
     dot_location = entry_string.find(".")
     chart_placement = int(entry_string[:dot_location])
-    remainder = entry_string[dot_location+1:]
+    remainder = entry_string[dot_location + 1:]
+
+    # parse previous week's placement
     ob = remainder.find("(")
     cb = remainder.find(")")
-    prev_week = convert_num(remainder[ob+1:cb])
-    remainder = remainder[cb+1:].strip()
-    ob = remainder.rfind("(")
-    cb = remainder.rfind(")")
-    publisher = remainder[ob+1:cb]
+    prev_week = convert_num(remainder[ob + 1:cb])
+    remainder = remainder[cb + 1:].strip()
+
+    # parse publisher, bail early if brackets mismatch
+    ob, cb = rfind_bracket_indices(remainder)
+    if ob is None or cb is None:
+        return bail_from_parsing(chart_placement=chart_placement,
+                                 prev_week=prev_week,
+                                 artist_and_song=remainder)
+    publisher = remainder[ob + 1:cb]
     remainder = remainder[:ob].strip()
-    ob = remainder.rfind("(")
-    cb = remainder.rfind(")")
+
+    # parse week in charts, bail early if brackets mismatch
+    ob, cb = rfind_bracket_indices(remainder)
+    if ob is None or cb is None:
+        return bail_from_parsing(chart_placement=chart_placement,
+                                 prev_week=prev_week,
+                                 publisher=publisher,
+                                 artist_and_song=remainder)
     week_in_charts = parse_week_in_charts(remainder[ob + 1:cb])
+
+    # parse artist and song
     artist_and_song = remainder[:ob].strip()
     artist_song_tuple = split_artist_and_song(artist_and_song)
     if artist_song_tuple is None:
-        artist = ""
-        song = ""
-        crawler_rejected = "yes"
+        return bail_from_parsing(chart_placement=chart_placement,
+                                 prev_week=prev_week,
+                                 publisher=publisher,
+                                 week_in_charts=week_in_charts,
+                                 artist_and_song=artist_and_song)
     else:
         artist = artist_song_tuple[0]
         song = artist_song_tuple[1]
-        crawler_rejected = "no"
+
+    # return parsed data
     return {'chart_placement': chart_placement,
             'previous_week': prev_week,
             'publisher': publisher,
@@ -79,7 +98,35 @@ def parse_entry(entry_string):
             'artist_and_song': artist_and_song,
             'artist': artist,
             'song': song,
-            'crawler_rejected': crawler_rejected}
+            'crawler_rejected': "no"}
+
+
+def bail_from_parsing(chart_placement=0, prev_week="", publisher="", week_in_charts=0, artist_and_song="", artist="",
+                      song=""):
+    return {'chart_placement': chart_placement,
+            'previous_week': prev_week,
+            'publisher': publisher,
+            'week_in_charts': week_in_charts,
+            'artist_and_song': artist_and_song,
+            'artist': artist,
+            'song': song,
+            'crawler_rejected': "yes"}
+
+
+def rfind_bracket_indices(string):
+    cb_index = -1
+    parity = 0
+    for i in range(len(string) - 1, -1, -1):
+        if cb_index < 0 and string[i] == ")":
+            cb_index = i
+        if string[i] == ")":
+            parity += 1
+        if string[i] == "(":
+            parity -= 1
+        if string[i] == "(" and parity == 0 and cb_index >= 0:
+            ob_index = i
+            return ob_index, cb_index
+    return None, None
 
 
 def convert_num(num_string):
@@ -138,12 +185,8 @@ def entries_to_entry_dict(clean_entry_strings, absolute_week, inquiry_date_dict)
 
 
 def append_one_week(dom, absolute_week, entries):
-    b_tags = dom.findAll("b")
-    assert len(b_tags) == 1
-    start_tag = b_tags[0]
-    assert len(start_tag.contents) == 1
-    start_tag_contents = str(start_tag.contents[0]).strip()
-    assert start_tag_contents == "Singles"
+    start_tag = find_singles_start_tag(dom.findAll("b"))
+    assert (start_tag is not None) and (str(start_tag.contents[0]).strip() == "Singles")
 
     collected_tags = collect_tags(start_tag)
     clean_entries = clean_tags(collected_tags)
@@ -153,28 +196,34 @@ def append_one_week(dom, absolute_week, entries):
         entries.append(entry)
 
 
-def write_to_file(entries, filename):
-    with open(filename, 'w', encoding="utf-8") as output_file:
+def find_singles_start_tag(b_tags):
+    for b_tag in b_tags:
+        if str(b_tag.contents[0]).strip() == "Singles":
+            return b_tag
+    return None
+
+
+def write_to_file(entries, filename, append=True):
+    file_modifier = "a" if append else "w"
+    with open(filename, file_modifier, encoding="utf-8") as output_file:
         keys = entries[0].keys()
         dict_writer = csv.DictWriter(output_file, keys, delimiter=',', lineterminator='\n')
-        dict_writer.writeheader()
+        if not append:
+            dict_writer.writeheader()
         dict_writer.writerows(entries)
 
 
-def bulk_download(start_week, end_week, out_filename, temp_filename=None, temp_out_frequency=0):
+def bulk_download(start_week, end_week, out_filename, temp_filename=None, temp_out_frequency=0, append=True):
     entries = []
     for absolute_week in range(start_week, end_week + 1):
         print("Crawling week {}".format(absolute_week))
         dom = fetch_week_dom(absolute_week)
         append_one_week(dom, absolute_week, entries)
         if temp_out_frequency != 0 and out_filename is not None and absolute_week % temp_out_frequency == 0:
-            write_to_file(entries, (temp_filename + "_weeks_{}-{}.csv".format(start_week, absolute_week)))
+            write_to_file(entries, (temp_filename + "_weeks_{}-{}.csv".format(start_week, absolute_week)), append=False)
         sleep(CRAWL_DELAY_S)
-    write_to_file(entries, (out_filename + "_weeks_{}-{}.csv".format(start_week, end_week)))
+    write_to_file(entries, (out_filename + ".csv"), append=True)
 
 
-# --- Breaking week --- #
-# dom = fetch_week_dom(2044)
-# append_one_week(dom, 2044, [])
-
-print("done")
+# bulk_download(1, 2566, "swiss_single_charts", "partial_swiss_single_charts", 200, append=True)
+bulk_download(2560, 2566, "last_test", append=True)
